6ee6023f4a80701c92399c6ddc4981fff45d9e92
Christian Poveda <christianpoveda@protonmail.com>
2019-04-01 01:29:51 -0500

    Add mechanism to recover MIR of all functions

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,56 @@
 extern crate rustc;
 extern crate rustc_driver;
 extern crate rustc_interface;
-extern crate rustc_mir;
 
-use rustc::hir::def_id::LOCAL_CRATE;
-use rustc::ty;
+use std::collections::HashMap;
+
+use rustc::hir::ItemKind;
+use rustc::mir::Mir;
+use rustc_driver::{report_ices_to_stderr_if_any, run_compiler, Callbacks};
 use rustc_interface::interface;
 
-use rustc_mir::interpret::EvalContext;
+fn find_sysroot() -> String {
+    let home = option_env!("RUSTUP_HOME").or(option_env!("MULTIRUST_HOME"));
+    let toolchain = option_env!("RUSTUP_TOOLCHAIN").or(option_env!("MULTIRUST_TOOLCHAIN"));
+
+    match (home, toolchain) {
+        (Some(home), Some(toolchain)) => format!("{}/toolchains/{}", home, toolchain),
+        _ => option_env!("RUST_SYSROOT")
+            .expect("could not find sysroot")
+            .to_owned(),
+    }
+}
 
 struct SireCompilerCalls;
 
-impl rustc_driver::Callbacks for SireCompilerCalls {
-    fn after_parsing(&mut self, compiler: &interface::Compiler) -> bool {
-        println!("We arrived after parsing");
+impl Callbacks for SireCompilerCalls {
+    fn after_parsing(&mut self, _compiler: &interface::Compiler) -> bool {
         true
     }
 
     fn after_analysis(&mut self, compiler: &interface::Compiler) -> bool {
-        println!("We arrived after analysis");
         compiler.session().abort_if_errors();
-
         compiler.global_ctxt().unwrap().peek_mut().enter(|tcx| {
-            let (main_id, _) = tcx.entry_fn(LOCAL_CRATE).expect("no main function found!");
-
-            let main_instance = ty::Instance::mono(tcx, main_id);
-            println!("{:?}", main_instance);
+            let hir = tcx.hir();
+            let mir_fns = hir
+                .krate()
+                .items
+                .iter()
+                .filter_map(|(node_id, item)| match item.node {
+                    ItemKind::Fn(_, _, _, _) => {
+                        let def_id = hir.local_def_id(*node_id);
+                        let path = tcx.def_path(def_id).to_filename_friendly_no_crate();
+                        let mir = tcx.optimized_mir(def_id);
+                        Some((path, mir))
+                    }
+                    _ => None,
+                })
+                .collect::<HashMap<String, &Mir>>();
+            println!("{:?}", mir_fns);
         });
 
         compiler.session().abort_if_errors();
         false
-    }
-}
-
-fn find_sysroot() -> String {
-    if let Ok(sysroot) = std::env::var("SIRE_SYSROOT") {
-        return sysroot;
-    }
-
-    // Taken from PR <https://github.com/Manishearth/rust-clippy/pull/911>.
-    let home = option_env!("RUSTUP_HOME").or(option_env!("MULTIRUST_HOME"));
-    let toolchain = option_env!("RUSTUP_TOOLCHAIN").or(option_env!("MULTIRUST_TOOLCHAIN"));
-    match (home, toolchain) {
-        (Some(home), Some(toolchain)) => format!("{}/toolchains/{}", home, toolchain),
-        _ => option_env!("RUST_SYSROOT")
-            .expect(
-                "could not find sysroot. Either set `MIRI_SYSROOT` at run-time, or at \
-                 build-time specify `RUST_SYSROOT` env var or use rustup or multirust",
-            )
-            .to_owned(),
     }
 }
 
@@ -63,8 +65,8 @@ fn main() {
         rustc_args.push(find_sysroot());
     }
 
-    let result = rustc_driver::report_ices_to_stderr_if_any(move || {
-        rustc_driver::run_compiler(&rustc_args, &mut SireCompilerCalls, None, None)
+    let result = report_ices_to_stderr_if_any(move || {
+        run_compiler(&rustc_args, &mut SireCompilerCalls, None, None)
     })
     .and_then(|result| result);
 
